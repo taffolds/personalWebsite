7aac241a1778375c6e3473ee7d53edbb182a2c1f
Thomas Tellefsen <thomasltellefsen@gmail.com>
2026-01-08 17:43:44 +0100

    made winning condition for row[0] matching row[1]

--- src/components/fourInARow.tsx
+++ src/components/fourInARow.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { Banner } from "./banner.js";
 // @ts-ignore
 import "../styles.css";
@@ -15,9 +15,35 @@ export function FourInARow() {
     [null, null, null, null, null, null, null],
   ]);
   const [turn, setTurn] = useState<CellValue>("red");
+  const [winner, setWinner] = useState<CellValue>(null);
+
+  useEffect(() => {
+    const checkWinner = (): CellValue => {
+      for (let rowIndex = 0; rowIndex < squares.length; rowIndex++) {
+        const row = squares[rowIndex];
+        if (row && row[0] !== null && row[0] === row[1]) {
+          console.log(`Winner is ${row[0]}`);
+          return row[0] as CellValue;
+        }
+      }
+      return null;
+    };
+
+    setWinner(checkWinner());
+  }, [squares]);
 
   function handleClick(rowIndex: number, columnIndex: number) {
     console.log(`Clicked cell at row: ${rowIndex}, column: ${columnIndex}`);
+
+    setSquares((old) => {
+      if (old[rowIndex]![columnIndex]) return old;
+
+      const updated = [...old];
+      updated[rowIndex] = [...updated[rowIndex]!];
+      updated[rowIndex][columnIndex] = turn;
+      setTurn((t) => (t === "red" ? "blue" : "red"));
+      return updated;
+    });
   }
 
   return (
@@ -29,13 +55,13 @@ export function FourInARow() {
         <tbody>
           {squares.map((row, rowIndex) => (
             <tr key={rowIndex}>
-              {row.map((column, columnIndex) => (
+              {row.map((r, columnIndex) => (
                 <td
                   key={columnIndex}
                   className="cells"
                   onClick={() => handleClick(rowIndex, columnIndex)}
                 >
-                  {`(${rowIndex}, ${columnIndex})`}
+                  {r || `(${rowIndex}, ${columnIndex})`}
                 </td>
               ))}
             </tr>
