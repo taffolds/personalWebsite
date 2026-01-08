6dbcfceb893e7e18f2aa49598b86a60e44d8f64e
Thomas Tellefsen <thomasltellefsen@gmail.com>
2026-01-08 16:46:35 +0100

    cells in four in a row are clickable, and tell which index is clicked

--- src/components/fourInARow.tsx
+++ src/components/fourInARow.tsx
@@ -1,11 +1,47 @@
-import React from "react";
+import React, { useState } from "react";
 import { Banner } from "./banner.js";
+// @ts-ignore
+import "../styles.css";
+
+type CellValue = null | "red" | "blue";
 
 export function FourInARow() {
+  const [squares, setSquares] = useState<CellValue[][]>([
+    [null, null, null, null, null, null, null],
+    [null, null, null, null, null, null, null],
+    [null, null, null, null, null, null, null],
+    [null, null, null, null, null, null, null],
+    [null, null, null, null, null, null, null],
+    [null, null, null, null, null, null, null],
+  ]);
+  const [turn, setTurn] = useState<CellValue>("red");
+
+  function handleClick(rowIndex: number, columnIndex: number) {
+    console.log(`Clicked cell at row: ${rowIndex}, column: ${columnIndex}`);
+  }
+
   return (
     <>
       <Banner />
       <h1>Game goes here</h1>
+
+      <table>
+        <tbody>
+          {squares.map((row, rowIndex) => (
+            <tr key={rowIndex}>
+              {row.map((column, columnIndex) => (
+                <td
+                  key={columnIndex}
+                  className="cells"
+                  onClick={() => handleClick(rowIndex, columnIndex)}
+                >
+                  {`(${rowIndex}, ${columnIndex})`}
+                </td>
+              ))}
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </>
   );
 }
